68a43a45ea492a7310d1f1eb641d3e1a86bdbeff
eric <eric_ji@aliyun.com>
2015-08-27 21:54:19 +0800

    增加消息设置首页

--- app/src/main/java/com/xj/guanquan/activity/user/QSystemSetActivity.java
+++ app/src/main/java/com/xj/guanquan/activity/user/QSystemSetActivity.java
@@ -2,11 +2,14 @@ package com.xj.guanquan.activity.user;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.xj.guanquan.R;
+import com.xj.guanquan.activity.message.QMsgSetActivity;
 import com.xj.guanquan.common.QBaseActivity;
 
 public class QSystemSetActivity extends QBaseActivity implements View.OnClickListener {
+    private RelativeLayout notifySet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,9 @@ public class QSystemSetActivity extends QBaseActivity implements View.OnClickLis
     protected void initView() {
         _setHeaderTitle(getString(R.string.title_activity_qsystem_set));
         _setRightHomeGone();
+
+        notifySet = (RelativeLayout) findViewById(R.id.notifySet);
+        notifySet.setOnClickListener(this);
     }
 
     @Override
@@ -27,6 +33,8 @@ public class QSystemSetActivity extends QBaseActivity implements View.OnClickLis
 
     @Override
     public void onClick(View v) {
-
+        if (v == notifySet) {
+            toActivity(QMsgSetActivity.class);
+        }
     }
 }
