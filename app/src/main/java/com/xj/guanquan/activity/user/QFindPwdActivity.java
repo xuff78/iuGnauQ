4dcba5e96b95153010b6f807e1147fb9d230444f
eric_jxx <jixiangxiang@infohold.com.cn>
2015-09-01 18:18:25 +0800

    捣鼓召回密码

--- app/src/main/java/com/xj/guanquan/activity/user/QFindPwdActivity.java
+++ app/src/main/java/com/xj/guanquan/activity/user/QFindPwdActivity.java
@@ -1,11 +1,15 @@
 package com.xj.guanquan.activity.user;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.xj.guanquan.R;
 import com.xj.guanquan.common.QBaseActivity;
 
-public class QFindPwdActivity extends QBaseActivity {
+public class QFindPwdActivity extends QBaseActivity implements OnClickListener {
+    private Button sendSms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +21,20 @@ public class QFindPwdActivity extends QBaseActivity {
     protected void initView() {
         _setRightHomeGone();
         _setHeaderTitle(getString(R.string.title_activity_qfind_pwd));
+
+        sendSms = (Button) findViewById(R.id.sendSms);
+        sendSms.setOnClickListener(this);
     }
 
     @Override
     protected void initHandler() {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == sendSms) {
+
+        }
     }
 }
